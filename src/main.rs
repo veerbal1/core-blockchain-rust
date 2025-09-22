c1b70fc479fb5cc1c40fbd18eb736665f56a79fc
veerbal1 <veerbalsingh1@gmail.com>
2025-09-22 13:06:24 +0530

    Step 1.2: Block Struct Define (Ownership and Basic Fields)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use std::time::{SystemTime, UNIX_EPOCH};
+
+type THash = [u8; 32];
+type TTimestamp = u64;
+type TData = Vec<u8>;
+type TNonce = u64;
+
+#[derive(Debug)]
+struct Block {
+    prev_hash: THash,      // Previous Block Hash
+    timestamp: TTimestamp, // Current time in seconds.
+    data: TData,           // Transaction's raw data
+    nonce: TNonce,         // Puzzle solution for mining
+    hash: THash,           // Apna calculated hash.
+}
+
 fn main() {
-    println!("Welcome to Core Blockchain Rust!");
+    let genesis_data = String::from("Genesis Block");
+    let timestamp = SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .unwrap()
+        .as_secs();
+
+    let mut genesis_block = Block {
+        prev_hash: [0u8; 32],
+        timestamp,
+        data: genesis_data.into_bytes(),
+        nonce: 0,
+        hash: [0u8; 32],
+    };
+
+    println!("{:?}", genesis_block);
 }
